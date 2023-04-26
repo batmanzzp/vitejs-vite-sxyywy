c3bf43e3c2bafe2d0b680faad60a699197c96a84
batmanzzp <54322666+batmanzzp@users.noreply.github.com>
2023-04-26 10:42:34 +0800

    2 files modified, 1 added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -13,6 +13,9 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <button @click="changeWeather">点击切换天气</button>
+    <h2>今天天气是{{ info }}</h2>
+
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -20,18 +23,33 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
     Install
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
+
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      isHot: true,
+    };
+  },
+  computed: {
+    info() {
+      return this.isHot ? '凉爽' : '炎热';
+    },
+  },
+  methods: {
+    changeWeather() {
+      this.isHot = !this.isHot;
+    },
+  },
+};
+</script>
 
 <style scoped>
 .read-the-docs {
